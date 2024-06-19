c561cbe110f7cfdf44ddc152de2115523d0103e2
tilic69 <23789@student.macleans.school.nz>
2024-06-19 14:39:20 +1200

    Init GUI

---
+++ gui.py
@@ -0,0 +1,31 @@
+import tkinter
+from tkinter import CENTER
+import tkinter.messagebox
+import customtkinter as tk
+
+tk.set_appearance_mode("system")
+tk.set_default_color_theme("blue")
+
+class app(tk.CTk):
+    def __init__(self):
+
+        super().__init__()
+        self.title("Photo Story")
+        self.geometry("600x360")
+
+        self.mainmenu = tk.CTkFrame(self)
+        self.mainmenu.grid_columnconfigure(0, weight=1)
+
+        self.mainTitle = tk.CTkLabel(self.mainmenu, text="One Click Story", font=tk.CTkFont("Segoe", 80, "normal"), fg_color="transparent")
+        self.startButton = tk.CTkButton(self.mainmenu, text="Start", font=tk.CTkFont("Segoe", 35, "normal"))
+        self.privacyButton = tk.CTkButton(self.mainmenu, text="Privacy", font=tk.CTkFont("Segoe", 35, "normal"))
+        self.exitButton = tk.CTkButton(self.mainmenu, text="Exit", font=tk.CTkFont("Segoe", 35, "normal"))
+
+        self.mainTitle.grid(row=0, column=0)
+        self.startButton.grid(row=1, column=0, pady=5)
+        self.privacyButton.grid(row=2, column=0, pady=5)
+        self.exitButton.grid(row=3, column=0, pady=5)
+
+        self.mainmenu.pack(anchor=CENTER, expand=True)
+
+app().mainloop()
