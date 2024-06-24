e24b698bd8b56d31b57f8984822bfdad62ed68ac
tilic69 <23789@student.macleans.school.nz>
2024-06-24 14:50:14 +1200

    make clikie exitie button workie

--- gui.py
+++ gui.py
@@ -19,7 +19,7 @@ class app(tk.CTk):
         self.mainTitle = tk.CTkLabel(self.mainmenu, text="One Click Story", font=tk.CTkFont("Segoe", 80, "normal"))
         self.startButton = tk.CTkButton(self.mainmenu, text="Start", font=tk.CTkFont("Segoe", 35, "normal"))
         self.privacyButton = tk.CTkButton(self.mainmenu, text="Privacy", font=tk.CTkFont("Segoe", 35, "normal"))
-        self.exitButton = tk.CTkButton(self.mainmenu, text="Exit", font=tk.CTkFont("Segoe", 35, "normal"))
+        self.exitButton = tk.CTkButton(self.mainmenu, text="Exit", font=tk.CTkFont("Segoe", 35, "normal"), command=self.mainmenu.quit)
 
         self.mainTitle.grid(row=0, column=0)
         self.startButton.grid(row=1, column=0, pady=5)
