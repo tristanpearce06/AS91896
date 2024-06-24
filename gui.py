8a75a0e569fdc9b323f99174af20cb20d169842e
tilic69 <23789@student.macleans.school.nz>
2024-06-24 15:13:15 +1200

    Set up codebase to allow for multiple pages/frames

--- gui.py
+++ gui.py
@@ -8,24 +8,62 @@ tk.set_default_color_theme("blue")
 
 class app(tk.CTk):
     def __init__(self):
-
         super().__init__()
+
         self.title("Photo Story")
         self.geometry("600x360")
 
-        self.mainmenu = tk.CTkFrame(self)
-        self.mainmenu.grid_columnconfigure(0, weight=1)
+        self.container = tk.CTkFrame(self)
+        self.container.pack(fill="both", expand=True)
 
-        self.mainTitle = tk.CTkLabel(self.mainmenu, text="One Click Story", font=tk.CTkFont("Segoe", 80, "normal"))
-        self.startButton = tk.CTkButton(self.mainmenu, text="Start", font=tk.CTkFont("Segoe", 35, "normal"))
-        self.privacyButton = tk.CTkButton(self.mainmenu, text="Privacy", font=tk.CTkFont("Segoe", 35, "normal"))
-        self.exitButton = tk.CTkButton(self.mainmenu, text="Exit", font=tk.CTkFont("Segoe", 35, "normal"), command=self.mainmenu.quit)
+        self.frames = {}
+
+        for Fr in (HomePage, PrivacyPage):
+            PageName = Fr.__name__
+            frame = Fr(parent=self.container, controller=self)
+            self.frames[PageName] = frame
+            frame.grid(row=0, column=0)
+
+        self.show_frame("HomePage")
+
+    def show_frame(self, pName):
+        frame = self.frames[pName]
+        frame.tkraise()
+
+class HomePage(tk.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        self.grid_columnconfigure(0, weight=1)
+
+        self.mainTitle = tk.CTkLabel(self, text="One Click Story", font=tk.CTkFont("Segoe", 80, "normal"))
+        self.startButton = tk.CTkButton(self, text="Start", font=tk.CTkFont("Segoe", 35, "normal"))
+        self.privacyButton = tk.CTkButton(self, text="Privacy", font=tk.CTkFont("Segoe", 35, "normal"), command=lambda:controller.show_frame("PrivacyPage"))
+        self.exitButton = tk.CTkButton(self, text="Exit", font=tk.CTkFont("Segoe", 35, "normal"), command=self.quit)
 
         self.mainTitle.grid(row=0, column=0)
         self.startButton.grid(row=1, column=0, pady=5)
         self.privacyButton.grid(row=2, column=0, pady=5)
         self.exitButton.grid(row=3, column=0, pady=5)
 
-        self.mainmenu.pack(anchor=CENTER, expand=True)
+        self.pack(anchor=CENTER, expand=True)
+
+
+class PrivacyPage(tk.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        self.grid_columnconfigure(0, weight=1)
+
+        self.mainTitle = tk.CTkLabel(self, text="One Click Story", font=tk.CTkFont("Segoe", 80, "normal"))
+        self.backButton = tk.CTkButton(self, text="Back", font=tk.CTkFont("Segoe", 35, "normal"), command=lambda:controller.show_frame("HomePage"))
+        self.exitButton = tk.CTkButton(self, text="Exit", font=tk.CTkFont("Segoe", 35, "normal"), command=self.quit)
+
+        self.mainTitle.grid(row=0, column=0)
+        self.backButton.grid(row=1, column=0, pady=5)
+        self.exitButton.grid(row=2, column=0, pady=5)
+
 
 app().mainloop()
