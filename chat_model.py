2321d746650b9e17c7bd15bddf8e75e628e39d78
tilic69 <23789@student.macleans.school.nz>
2024-06-25 14:12:31 +1200

    Fix chat model to work better and fix an error in std_func that means that it only listed 1 of each type of object

--- chat_model.py
+++ chat_model.py
@@ -20,6 +20,11 @@ print(detected_obj)
 
 from openai import OpenAI
 
+modified_img = object_rec.modifyImage(modelresults, frame)
+
+object_rec.displayWindow(modified_img)
+
+
 print("generating story")
 
 client = OpenAI(
@@ -30,8 +35,8 @@ client = OpenAI(
 chat_completion = client.chat.completions.create(
     model="tgi",
     messages=[
-        {"role": "system", "content": "You are a short story teller. Create a short story, less than 200 characters long, using ONLY the given items. Ensure that the correct amount of items are always present in the story. The format for the items given is (amount:object)" },
-        {"role": "user", "content": f"{detected_obj}"}
+        {"role": "system", "content": "You are a short story teller. Create a short story, less than 200 characters long, using ONLY the given items. Ensure that the correct amount of items are always present in the story. Do not create objects or people that are not provided to you. The format for the items given is (amount:object)." },
+        {"role": "user", "content": f"Create a story using these objects:{detected_obj}. DO NOT add anything to the story that is not in this list!"}
     ],
     stream=True
 )
--- std_func.py
+++ std_func.py
@@ -1,6 +1,6 @@
 def std_dict_addition(dict, obj):
     if obj in dict:
-        dict[obj] =+ 1
+        dict[obj] += 1
     else:
         dict[obj] = 1
     return dict
