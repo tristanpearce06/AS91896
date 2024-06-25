e07be7f4378a3052fdbf1ce6a5ebe4d1a678e40e
tilic69 <23789@student.macleans.school.nz>
2024-06-26 11:50:04 +1200

    Create initial chat model code formatted as a callable function and test integration with object recognition module

--- chat_model.py
+++ chat_model.py
@@ -1,45 +1,18 @@
-import object_rec
-
-print("Capturing frame")
-
-ret, frame = object_rec.captureFrame()
-
-print("frame captured")
-print("creating model")
-
-modelresults = object_rec.model(frame)
-
-print("model created")
-print("finding objects")
-
-detected_obj = object_rec.returnFoundObjects(modelresults)
-
-print("objects found")
-
-print(detected_obj)
-
 from openai import OpenAI
 
-modified_img = object_rec.modifyImage(modelresults, frame)
+def gen_story(items:dict):
+    client = OpenAI(
+        base_url="https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct/v1",
+        api_key="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"
+    )
 
-object_rec.displayWindow(modified_img)
+    chat_completion = client.chat.completions.create(
+        model="tgi",
+        messages=[
+            {"role": "system", "content": "You are a short story teller. Create a short story, less than 200 characters long, using ONLY the given items. Ensure that the correct amount of items are always present in the story. Do not create objects or people that are not provided to you. The format for the items given is (amount:object)." },
+            {"role": "user", "content": f"Create a story using these objects:{items}. DO NOT add anything to the story that is not in this list!"}
+        ],
+        stream=False
+    )
 
-
-print("generating story")
-
-client = OpenAI(
-    base_url="https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct/v1",
-    api_key="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"
-)
-
-chat_completion = client.chat.completions.create(
-    model="tgi",
-    messages=[
-        {"role": "system", "content": "You are a short story teller. Create a short story, less than 200 characters long, using ONLY the given items. Ensure that the correct amount of items are always present in the story. Do not create objects or people that are not provided to you. The format for the items given is (amount:object)." },
-        {"role": "user", "content": f"Create a story using these objects:{detected_obj}. DO NOT add anything to the story that is not in this list!"}
-    ],
-    stream=True
-)
-
-for message in chat_completion:
-    print(message.choices[0].delta.content, end="")
+    return chat_completion.choices[0].message.content
--- testfile.py
+++ testfile.py
@@ -1,14 +1,40 @@
 # Object recognition module test
 
 import object_rec
+import chat_model
+
+# ret, frame = object_rec.captureFrame()
+
+# modelresults = object_rec.model(frame)
+
+# detected_obj = object_rec.returnFoundObjects(modelresults)
+
+# modified_img = object_rec.modifyImage(modelresults, frame)
+
+# object_rec.displayWindow(modified_img)
+
+# Chat module + object recongition module test
+
+print("Capturing frame")
 
 ret, frame = object_rec.captureFrame()
 
+print("frame captured")
+print("creating model")
+
 modelresults = object_rec.model(frame)
 
+print("model created")
+print("finding objects")
+
 detected_obj = object_rec.returnFoundObjects(modelresults)
+
+print("objects found")
+
+print(detected_obj)
 
 modified_img = object_rec.modifyImage(modelresults, frame)
 
 object_rec.displayWindow(modified_img)
 
+print(chat_model.gen_story(detected_obj))
