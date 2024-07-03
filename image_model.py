a6fae1c292a3917d77d79b445c50307c9cfd29c0
tilic69 <23789@student.macleans.school.nz>
2024-07-03 13:53:01 +1200

    remove stencil

--- image_model.py
+++ image_model.py
@@ -10,7 +10,7 @@ headers = {
 }
 
 def generate_image_from_text(prompt):
-    modifiedPrompt = f"{prompt}, ((art, fantasy, unrealistic, stencil)"
+    modifiedPrompt = f"{prompt}, ((art, fantasy, unrealistic)"
     payload = {
         "inputs": modifiedPrompt,
         "negative_prompt": "out of frame, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature"
