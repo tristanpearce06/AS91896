e6266b3af02ddd243f2897e3a410e168b63145e3
tilic69 <23789@student.macleans.school.nz>
2024-07-03 15:14:19 +1200

    Added basic object recognition page (NOT DONE)

--- gui.py
+++ gui.py
@@ -22,7 +22,7 @@ class app(tk.CTk):
 
         self.frames = {}
 
-        for Fr in (HomePage, PrivacyPage, ImageInputPage, InputSelect):
+        for Fr in (HomePage, PrivacyPage, ImageInputPage, InputSelect, ObjectRecPage):
             PageName = Fr.__name__
             frame = Fr(parent=self.container, controller=self)
             self.frames[PageName] = frame
@@ -102,7 +102,7 @@ class ImageInputPage(tk.CTkFrame):
         self.uploadedImage = tk.CTkLabel(self.imageHolder, width=400, height=300, text="")
 
         self.imageButton = tk.CTkButton(self.centerFrame, text="Upload Image", font=tk.CTkFont("Segoe", 20, "normal"), command=self.imageUpload)
-        self.continueButton = tk.CTkButton(self.centerFrame, text="Continue", font=tk.CTkFont("Segoe", 20, "normal"), command=self.quit, state="disabled") # Default state is disable to ensure that user does not continue without an uploaded image
+        self.continueButton = tk.CTkButton(self.centerFrame, text="Continue", font=tk.CTkFont("Segoe", 20, "normal"), command=lambda:controller.show_frame("ObjectRecPage"), state="disabled") # Default state is disable to ensure that user does not continue without an uploaded image
         self.exitButton = tk.CTkButton(self.centerFrame, text="Exit", font=tk.CTkFont("Segoe", 20, "normal"), command=self.quit)
         
         #self.mainTitle.place(relx=0.5, rely=0.1, anchor=CENTER)
@@ -152,5 +152,38 @@ class InputSelect(tk.CTkFrame):
         self.cameraInput.pack(padx=15, pady=5)
         self.backButton.pack(padx=15, pady=5)
         self.exitButton.pack(padx=15, pady=(5, 15))
+
+class ObjectRecPage(tk.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+
+        self.mainTitle = tk.CTkLabel(self, text="Object Recognition", font=tk.CTkFont("Segoe", 60, "normal"))
+        
+        self.centerFrame = tk.CTkFrame(self, border_width=1)
+        self.centerFrame.grid_columnconfigure(0, weight=1)
+
+        self.imageHolder = tk.CTkFrame(self.centerFrame, width=400, height=300)
+        self.uploadedImage = tk.CTkLabel(self.imageHolder, width=400, height=300, text="")
+
+        self.continueButton = tk.CTkButton(self.centerFrame, text="Continue", font=tk.CTkFont("Segoe", 20, "normal"), command=self.objectRec)
+        self.backButton = tk.CTkButton(self.centerFrame, text="Back", font=tk.CTkFont("Segoe", 20, "normal"), command=lambda:controller.show_frame("HomePage"))
+        self.exitButton = tk.CTkButton(self.centerFrame, text="Exit", font=tk.CTkFont("Segoe", 20, "normal"), command=self.quit)
+
+        self.mainTitle.place(relx=0.5, rely=0.1, anchor=CENTER)
+        
+        self.centerFrame.grid(row=2, column=0)
+        self.imageHolder.pack(padx=15, pady=(15, 5))
+        self.uploadedImage.pack(expand=True, fill="both")
+
+        self.continueButton.pack(padx=15, pady=(15, 5))
+        self.backButton.pack(padx=15, pady=5)
+        self.exitButton.pack(padx=15, pady=(5, 15))
+
+    def objectRec(image):
+        print("test")
 
 app().mainloop()
